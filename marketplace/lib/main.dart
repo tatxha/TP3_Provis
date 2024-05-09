b8a45a67a3707bf28c99710eeb106cb034e7ff2e
tatxha <tatthamaharany@gmail.com>
2024-05-09 18:22:16 +0700

    nambahin page home yang berisi daftar produk

--- marketplace/lib/main.dart
+++ marketplace/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Produk'),
     );
   }
 }
@@ -54,72 +54,106 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class Product {
+  final String name;
+  final String imagePath;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  Product({required this.name, required this.imagePath});
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  List<Product> products = [
+    Product(name: 'Product 1', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 2', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 3', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 4', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 5', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 6', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 7', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 8', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 9', imagePath: 'assets/image1.jpg'),
+    Product(name: 'Product 10', imagePath: 'assets/image1.jpg'),
+    // Add more products here
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // Mendapatkan informasi tentang ukuran layar perangkat
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    // Menghitung lebar widget responsif
+    final productWidth = (screenWidth - 24) / 2; // Mengurangi margin dari total lebar layar
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: GridView.count(
+        crossAxisCount: 2, // biar setiap row hanya nampilin 2 produk
+        childAspectRatio: productWidth / (productWidth + 100), // atur tinggi rownya
+        // loop data produk
+        children: List.generate(products.length, (index) {
+          final product = products[index];
+          return Container( // container produk
+            width: productWidth,
+            // height: 500,
+            // height: MediaQuery.of(context).size.width * 2.5,
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8.0),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  product.imagePath, // Sesuaikan dengan path gambar Anda
+                  width: productWidth,
+                  // width: 100, // Sesuaikan dengan lebar gambar yang diinginkan
+                  // height: 100, // Sesuaikan dengan tinggi gambar yang diinginkan
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  product.name,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                SizedBox(height: 8.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  child: Text('Add to Cart'),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        }),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Chart',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.deepPurple,
+        onTap: _onItemTapped,
+      ),
     );
+  }
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
